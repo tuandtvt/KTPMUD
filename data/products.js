3be797ddb1e07dcd86af0f5f39798e6ed62110bf
Trong Tuan <tuandtvt2002@gmail.com>
2023-02-17 00:00:44 +0700

    update

---
+++ data/products.js
@@ -0,0 +1,374 @@
+var list_products = [
+    {
+    
+    "name": "Dam Hong",
+    "company": "San5",
+    "img": "img/san-bong-co-nhan-tao-khu-noi-tru-viet-tri-1.jpg",
+    "price": "500000",
+    "star": 3.8,
+    "rateCount": 23,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 5",
+        "denchieusang": "Trắng",
+        "camara": "Tiêu chuẩn sân 5",
+        "camaraFront": "Có",
+        "cpu": "Nhân tạo",
+        "ram": "Động lực",
+        "rom": "16h-17h",
+        "microUSB": "Không",
+        "battery": "Tùy chọn"
+    },
+    "masp": "Dam0"
+},
+{
+    
+    "name": "Dam Hong 1",
+    "company": "San5",
+    "img": "img/san-bong-co-nhan-tao-khu-noi-tru-viet-tri-1.jpg",
+    "price": "500000",
+    "star": 3.8,
+    "rateCount": 23,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 5",
+        "denchieusang": "Trắng",
+        "camara": "Tiêu chuẩn sân 5",
+        "camaraFront": "Có",
+        "cpu": "Nhân tạo",
+        "ram": "Động lực",
+        "rom": "16h-17h",
+        "microUSB": "Không",
+        "battery": "Tùy chọn"
+    },
+    "masp": "Dam"
+},
+{
+    
+    "name": "Hoang Mai",
+    "company": "San5",
+    "img": "img/hiokayamaground01.jpg",
+    "price": "800000",
+    "star": 3.8,
+    "rateCount": 23,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 5",
+        "denchieusang": "Trắng",
+        "camara": "Tiêu chuẩn sân 5",
+        "camaraFront": "Có",
+        "cpu": "Nhân tạo",
+        "ram": "Động lực",
+        "rom": "18h-20h",
+        "microUSB": "Không",
+        "battery": "Tùy chọn"
+    },
+    "masp": "Hoa0"
+},
+{
+    
+    "name": "Hoang Cau",
+    "company": "San5",
+    "img": "img/den-san-bong.jpg",
+    "price": "350000",
+    "star": 4.3,
+    "rateCount": 43,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 5",
+        "denchieusang": "Trắng",
+        "camara": "Tiêu chuẩn sân 5",
+        "camaraFront": "Có",
+        "cpu": "Nhân tạo",
+        "ram": "Adidas",
+        "rom": "15h-16h",
+        "microUSB": "Không",
+        "battery": "Tùy chọn"
+    },
+    "masp": "Hoa1"
+},
+
+{
+    
+    "name": "Den Lu",
+    "company": "San5",
+    "img": "img/6347AE891EAF60A7B940983BFDAE17A6.jpg",
+    "price": "550000",
+    "star": 4.4,
+    "rateCount": 34,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 5",
+        "denchieusang": "Trắng",
+        "camara": "Tiêu chuẩn sân 5",
+        "camaraFront": "Có",
+        "cpu": "Nhân tạo",
+        "ram": "Động lực",
+        "rom": "17h30-18h30",
+        "microUSB": "Không",
+        "battery": "Tùy chọn"
+    },
+    "masp": "Den"
+},
+{
+    
+    "name": "Thanh Xuan",
+    "company": "San5",
+    "img": "img/co-nhan-tao-mini.jpg",
+    "price": "500000",
+    "star": 4.6,
+    "rateCount": 45,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 5",
+        "denchieusang": "Trắng",
+        "camara": "Tiêu chuẩn sân 5",
+        "camaraFront": "Có",
+        "cpu": "Nhân tạo",
+        "ram": "Adidas",
+        "rom": "1h-2h",
+        "microUSB": "Không",
+        "battery": "Tùy chọn"
+    },
+    "masp": "Tha"
+},
+{
+    
+    "name": "Lang Thuong",
+    "company": "San7CT",
+    "img": "img/san-bong-da-phuc-loc-619027.jpg",
+    "price": "600000",
+    "star": 4.6,
+    "rateCount": 45,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 7",
+        "denchieusang": "Trắng",
+        "camara": "Tiêu chuẩn sân 7",
+        "camaraFront": "Có",
+        "cpu": "Thật",
+        "ram": "Adidas",
+        "rom": "16h-17h30",
+        "microUSB": "Không",
+        "battery": "Tùy chọn"
+    },
+    "masp": "Lag"
+},
+{
+    
+    "name": "Lang Ha",
+    "company": "San7CT",
+    "img": "img/sanbongxiengkhuang.jpg",
+    "price": "6500000",
+    "star": 4.6,
+    "rateCount": 55,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 7",
+        "denchieusang": "Trắng",
+        "camara": "Tiêu chuẩn sân 7",
+        "camaraFront": "Có",
+        "cpu": "Thật",
+        "ram": "Adidas",
+        "rom": "16h-17h30",
+        "microUSB": "Không",
+        "battery": "Tùy chọn"
+    },
+    "masp": "Lag1"
+},{
+    
+    "name": "Trung Van",
+    "company": "San7CT",
+    "img": "img/san-bong-co-nhan-tao-nen-chu-y-chat-luong-co-768x576.jpg",
+    "price": "700000",
+    "star": 4.8,
+    "rateCount": 58,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 7",
+        "denchieusang": "Vàng",
+        "camara": "Tiêu chuẩn sân 7",
+        "camaraFront": "Có",
+        "cpu": "Thật",
+        "ram": "Adidas",
+        "rom": "1630h-18h30",
+        "microUSB": "Không",
+        "battery": "Tùy chọn"
+    },
+    "masp": "Cau"
+},
+{
+    
+    "name": "Binh Minh",
+    "company": "San7CT",
+    "img": "img/IMG_20170407_133212.jpg",
+    "price": "750000",
+    "star": 4.8,
+    "rateCount": 85,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 7",
+        "denchieusang": "Vàng",
+        "camara": "Tiêu chuẩn sân 7",
+        "camaraFront": "Có",
+        "cpu": "Thật",
+        "ram": "Động lực",
+        "rom": "18h30-20h30",
+        "microUSB": "Không",
+        "battery": "Tùy chọn"
+    },
+    "masp": "Bin"
+},
+{
+    
+    "name": "Kinh Te",
+    "company": "San7NT",
+    "img": "img/chieu-dai-san-bong-da-2.jpg",
+    "price": "750000",
+    "star": 4.8,
+    "rateCount": 85,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 7",
+        "denchieusang": "Vàng",
+        "camara": "Tiêu chuẩn sân 7",
+        "camaraFront": "Có",
+        "cpu": "Nhân tạo",
+        "ram": "Động lực",
+        "rom": "18h30-20h30",
+        "microUSB": "Không",
+        "battery": "Tùy chọn"
+    },
+    "masp": "Kin"
+},
+{
+    
+    "name": "Xay dung",
+    "company": "San7NT",
+    "img": "img/pexels-photo-186239.jpeg",
+    "price": "800000",
+    "star": 4.8,
+    "rateCount": 85,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 7",
+        "denchieusang": "Vàng",
+        "camara": "Tiêu chuẩn sân 7",
+        "camaraFront": "Có",
+        "cpu": "Nhân tạo",
+        "ram": "Adidas",
+        "rom": "18h30-20h30",
+        "microUSB": "Không",
+        "battery": "Tùy chọn"
+    },
+    "masp": "Xay"
+},{
+    
+    "name": "Tam Trinh",
+    "company": "San11CT",
+    "img": "img/pexels-photo-3459633.jpeg",
+    "price": "1500000",
+    "star": 4.8,
+    "rateCount": 25,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 11",
+        "denchieusang": "Trắng",
+        "camara": "Tiêu chuẩn sân 11",
+        "camaraFront": "Có",
+        "cpu": "Cỏ thật",
+        "ram": "Adidas",
+        "rom": "18h30-20h30",
+        "microUSB": "Có",
+        "battery": "Có"
+    },
+    "masp": "Tam"
+},{
+    
+    "name": "Yen So",
+    "company": "San11CT",
+    "img": "img/istockphoto-693252158-2048x2048.jpg",
+    "price": "2500000",
+    "star": 4.7,
+    "rateCount": 12,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 11",
+        "denchieusang": "Trắng",
+        "camara": "Tiêu chuẩn sân 11",
+        "camaraFront": "Có",
+        "cpu": "Cỏ thật",
+        "ram": "Adidas",
+        "rom": "18h30-20h30",
+        "microUSB": "Có",
+        "battery": "Có"
+    },
+    "masp": "Yen"
+},{
+    
+    "name": "My Dinh",
+    "company": "San11NT",
+    "img": "img/San-van-dong-My-Dinh-Lich-su-hinh-thanh-kien-truc-suc-chua-banner.jpg",
+    "price": "2000000",
+    "star": 4.2,
+    "rateCount": 22,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "kichThuocSan": "Tiêu chuẩn sân 11",
+        "denchieusang": "Trắng",
+        "camara": "Tiêu chuẩn sân 11",
+        "camaraFront": "Có",
+        "cpu": "Cỏ thật",
+        "ram": "Adidas",
+        "rom": "18h30-20h30",
+        "microUSB": "Có",
+        "battery": "Có"
+    },
+    "masp": "MyD"
+},
+ ]
---
+++ js/giohang.js
@@ -0,0 +1,195 @@
+var currentuser; // user hiện tại, biến toàn cục
+window.onload = function () {
+    khoiTao();
+
+	// autocomplete cho khung tim kiem
+	autocomplete(document.getElementById('search-box'), list_products);
+
+	// thêm tags (từ khóa) vào khung tìm kiếm
+	var tags = ["Sân 5", "Sân 7 CT", "Sân 7 CT", "Sân 11 CT", "Sân 11 NT"];
+	for (var t of tags) addTags(t, "index.html?search=" + t)
+
+	currentuser = getCurrentUser();
+	addProductToTable(currentuser);
+}
+
+function addProductToTable(user) {
+	var table = document.getElementsByClassName('listSanPham')[0];
+
+	var s = `
+		<tbody>
+			<tr>
+				<th>STT</th>
+				<th>Sân</th>
+				<th>Giá</th>
+				<th>Khung giờ</th>
+				<th>Thành tiền</th>
+				<th>Thời gian đặt sân</th>
+				<th>Xóa</th>
+			</tr>`;
+
+	if (!user) {
+		s += `
+			<tr>
+				<td colspan="7"> 
+					<h1 style="color:red; background-color:white; font-weight:bold; text-align:center; padding: 15px 0;">
+						Bạn chưa đăng nhập !!
+					</h1> 
+				</td>
+			</tr>
+		`;
+		table.innerHTML = s;
+		return;
+	} else if (user.products.length == 0) {
+		s += `
+			<tr>
+				<td colspan="7"> 
+					<h1 style="color:green; background-color:white; font-weight:bold; text-align:center; padding: 15px 0;">
+						Đơn rỗng !!
+					</h1> 
+				</td>
+			</tr>
+		`;
+		table.innerHTML = s;
+		return;
+	}
+
+	var totalPrice = 0;
+	for (var i = 0; i < user.products.length; i++) {
+		var masp = user.products[i].ma;
+		var soluongSp = user.products[i].soluong;
+		var p = timKiemTheoMa(list_products, masp);
+		var price = (p.promo.name == 'giareonline' ? p.promo.value : p.price);
+		var thoigian = new Date(user.products[i].date).toLocaleString();
+		var thanhtien = stringToNum(price) * 1;
+
+		s += `
+			<tr>
+				<td>` + (i + 1) + `</td>
+				<td class="noPadding imgHide">
+					<a target="_blank" href="chitietsanpham.html?` + p.name.split(' ').join('-') + `" title="Xem chi tiết">
+						` + p.name + `
+						<img src="` + p.img + `">
+					</a>
+				</td>
+				<td class="alignRight">` + price + ` ₫</td>
+				<td class="soluong" >
+					<button onclick="giamSoLuong('` + masp + `')"><i class="fa fa-minus"></i></button>
+					<input size="1" onchange="capNhatSoLuongFromInput(this, '` + masp + `')" value=` + soluongSp + `>
+					<button onclick="tangSoLuong('` + masp + `')"><i class="fa fa-plus"></i></button>
+				</td>
+				<td class="alignRight">` + numToString(thanhtien) + ` ₫</td>
+				<td style="text-align: center" >` + thoigian + `</td>
+				<td class="noPadding"> <i class="fa fa-trash" onclick="xoaSanPhamTrongGioHang(` + i + `)"></i> </td>
+			</tr>
+		`;
+		// Chú ý nháy cho đúng ở giamsoluong, tangsoluong
+		totalPrice += thanhtien;
+	}
+
+	s += `
+			<tr style="font-weight:bold; text-align:center">
+				<td colspan="4">TỔNG TIỀN: </td>
+				<td class="alignRight">` + numToString(totalPrice) + ` ₫</td>
+				<td class="thanhtoan" onclick="thanhToan()"> Xác nhận </td>
+				<td class="xoaHet" onclick="xoaHet()"> Xóa hết </td>
+			</tr>
+		</tbody>
+	`;
+
+	table.innerHTML = s;
+}
+
+function xoaSanPhamTrongGioHang(i) {
+	if (window.confirm('Xác nhận hủy mua')) {
+		currentuser.products.splice(i, 1);
+		capNhatMoiThu();
+	}
+}
+
+function thanhToan() {
+	var c_user = getCurrentUser();
+	if(c_user.off) {
+        alert('Tài khoản của bạn hiện đang bị khóa nên không thể đặt!');
+        addAlertBox('Tài khoản của bạn đã bị khóa bởi Admin.', '#aa0000', '#fff', 10000);
+        return;
+	}
+	
+	if (!currentuser.products.length) {
+		addAlertBox('Không có sân nào cần xác nhận !!', '#ffb400', '#fff', 2000);
+		return;
+	}
+	if (window.confirm('Thanh toán đơn ?')) {
+		var order = {
+			"sp": currentuser.products,
+			"ngaymua": new Date(),
+			"tinhTrang": 'Đang chờ xử lý'
+		}
+		console.log("order",order)
+		currentuser.donhang.push(order);
+		currentuser.products = [];
+		capNhatMoiThu();
+		addAlertBox('Các sân đã được gửi vào đơn và chờ xử lý.', '#17c671', '#fff', 4000);
+	}
+}
+
+function xoaHet() {
+	if (currentuser.products.length) {
+		if (window.confirm('Bạn có chắc chắn muốn xóa hết sân trong đơn !!')) {
+			currentuser.products = [];
+			capNhatMoiThu();
+		}
+	}
+}
+
+// Cập nhật số lượng lúc nhập số lượng vào input
+function capNhatSoLuongFromInput(inp, masp) {
+	var soLuongMoi = Number(inp.value);
+	if (!soLuongMoi || soLuongMoi <= 0) soLuongMoi = 1;
+
+	for (var p of currentuser.products) {
+		if (p.ma == masp) {
+			p.soluong = soLuongMoi;
+		}
+	}
+
+	capNhatMoiThu();
+}
+
+function tangSoLuong(masp) {
+	for (var p of currentuser.products) {
+		if (p.ma == masp) {
+			p.soluong++;
+		}
+	}
+
+	capNhatMoiThu();
+}
+
+function giamSoLuong(masp) {
+	for (var p of currentuser.products) {
+		if (p.ma == masp) {
+			if (p.soluong > 1) {
+				p.soluong--;
+			} else {
+				return;
+			}
+		}
+	}
+
+	capNhatMoiThu();
+}
+
+function capNhatMoiThu() { // Mọi thứ
+	animateCartNumber();
+
+	// cập nhật danh sách sản phẩm trong localstorage
+	setCurrentUser(currentuser);
+	updateListUser(currentuser);
+
+	// cập nhật danh sách sản phẩm ở table
+	addProductToTable(currentuser);
+
+	// Cập nhật trên header
+	capNhat_ThongTin_CurrentUser();
+}
